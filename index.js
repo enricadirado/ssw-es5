1e9fee095fed1857c8faa40221982554ae7e67de
enricadirado <100517394+enricadirado@users.noreply.github.com>
2023-06-10 16:29:40 +0200

    1 file modified

--- index.js
+++ index.js
@@ -11,31 +11,33 @@ associare una funzionalità di ricerca al digitare una lettera ed eseguire le ri
 ogni volta che premo una lettera viene visualizzato il match o il fatto che ci sono tanti match.
 
 */
-const URL = 'https://eu-central-1.aws.data.mongodb-api.com/app/kvaas-giwjg/endpoint/get?key=3abb610b';
+const URL =
+  'https://eu-central-1.aws.data.mongodb-api.com/app/kvaas-giwjg/endpoint/get?key=3abb610b';
 const result = document.getElementById('result');
 const btnInput = document.getElementById('btnInput');
-let database='';
+let database = '';
 
-
-btnInput.addEventListener('keyup', function(){
-  const inputText= btnInput.value;
-  fetch(URL).then(
-    (response) => response.json(), // parsing per avere la stringa
-    (error) => alert(error)
-  ).then((data) => {
-    /*console.log(data);*/
-    const db = JSON.parse(data); // parsing per avere l’array
-    /*console.log('1 db', db);*/    
-    const archivio = db.filter(myFun, btnInput.value);
-    console.log('archivioFINE', archivio);
-    if(archivio.length>1){
-      result.innerHTML="La ricerca ha prodotto "+ archivio.length + " risultati."
-    } else if (archivio.length==1){
-      result.innerHTML="Autore: "+ archivio[0].autore + "\nTitolo: "+ archivio[0].titolo;
-    } else if (archivio.length==0){
-      result.innerHTML="La ricerca non ha prodotto nessun risultato. "
-    }
-  });  
+btnInput.addEventListener('keyup', function () {
+  const inputText = btnInput.value;
+  fetch(URL)
+    .then(
+      (response) => response.json(), // parsing per avere la stringa
+      (error) => alert(error)
+    )
+    .then((data) => {
+      const db = JSON.parse(data); // parsing per avere l’array
+      const archivio = db.filter(myFun, btnInput.value);
+      console.log('archivioFINE', archivio);
+      if (archivio.length > 1) {
+        result.innerHTML =
+          'La ricerca ha prodotto ' + archivio.length + ' risultati.';
+      } else if (archivio.length == 1) {
+        result.innerHTML =
+          'Autore: ' + archivio[0].autore + '\nTitolo: ' + archivio[0].titolo;
+      } else if (archivio.length == 0) {
+        result.innerHTML = 'La ricerca non ha prodotto nessun risultato. ';
+      }
+    });
 });
 
 /*filter() crea un nuovo array con gli elementi che passano un test della funzione */
@@ -46,21 +48,13 @@ btnInput.addEventListener('keyup', function(){
 
 - devono trovabili anche con minuscolo
 */
-function myFun(value){
+function myFun(value) {
   console.log('btn', this);
-  /*console.log('test', pattern.test("ma"));*/
-  //const pattern = (/(\w*\s)*\w*/);
-  /*const str= value.titolo.concat(value.autore).toLocaleLowerCase();*/
-  if (value.titolo.match(this)){
-    console.log('this', this);
-    console.log('value', value);
-    console.log('titolo', value.titolo);
+  if (value.titolo.match(this)) {
+    console.log('this:', this, ' value: ', value, ' titolo:', value.titolo);
     return value.titolo;
-  } 
+  }
 }
-
-
-
 
 /*
 function myFun(btn){
@@ -78,12 +72,23 @@ function myFun(btn){
   }
 };
 
-
-
 &&(pattern.test(this)
+
+
+
+function myFun(value){
+  console.log('btn', this);
+
+  console.log('test', pattern.test("ma"));*/
+//const pattern = (/(\w*\s)*\w*/);
+/*const str= value.titolo.concat(value.autore).toLocaleLowerCase();
+
+  if (value.titolo.match(this)){
+    console.log('this', this);
+    console.log('value', value);
+    console.log('titolo', value.titolo);
+    return value.titolo;
+  } 
+}
+
 */
-  
-
-
-
-
